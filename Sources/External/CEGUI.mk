b70e15df4e7f7aee4c1e910243997a4c33ca4298
Piotr Podusowski <podusowski@gmail.com>
2011-10-28 12:54:43 +0200

    cegui build fix

--- Sources/External/CEGUI.mk
+++ Sources/External/CEGUI.mk
@@ -2,9 +2,17 @@ CEGUI_DIR:=$(BUILD)/CEGUI-0.7.5
 CEGUI_LIB:=$(CEGUI_DIR)/cegui/src/.libs/libCEGUIBase.so
 CEGUI_CONFIGURE:=$(CEGUI_DIR)/configure
 CEGUI_PACKAGE:=CEGUI-0.7.5.tar.gz
+CEGUI_PACKAGE_BUILD:=$(BUILD)/$(CEGUI_PACKAGE)
 
 .PHONY: CEGUI
-CEGUI: $(CEGUI_LIB) Deploy
+CEGUI: $(CEGUI_LIB) CopyLibraries
+
+.PHONY: CopyLibraries 
+CopyLibraries: $(CEGUI_LIB)
+	@find $(CEGUI_DIR) -name "*.so" -exec cp -lf {} $(BUILD)/ \;
+
+$(CEGUI_LIB): $(CEGUI_DIR)/config.status
+	cd $(CEGUI_DIR) && make -j2 -s
 
 $(CEGUI_DIR)/config.status: $(CEGUI_CONFIGURE)
 	cd $(CEGUI_DIR) &&\
@@ -18,16 +26,12 @@ $(CEGUI_DIR)/config.status: $(CEGUI_CONFIGURE)
 		--disable-stb \
 		--disable-libxml
 
-$(CEGUI_LIB): $(CEGUI_DIR)/config.status
-	cd $(CEGUI_DIR) && make -j2 -s
+$(CEGUI_CONFIGURE): $(CEGUI_PACKAGE_BUILD)
+	cd $(BUILD) && tar --touch -xf $(CEGUI_PACKAGE_BUILD) 
 
-$(CEGUI_CONFIGURE): $(CEGUI_PACKAGE)
-	@echo "unpacking CEGUI"
-	cp $(CEGUI_PACKAGE) $(BUILD)
-	cd $(BUILD) && tar xf $(CEGUI_PACKAGE) 
+$(CEGUI_PACKAGE_BUILD): $(CEGUI_PACKAGE)
+	mkdir -p $(BUILD)
+	@echo cp $(CEGUI_PACKAGE) $(CEGUI_PACKAGE_BUILD)
+	cp $(CEGUI_PACKAGE) $(CEGUI_PACKAGE_BUILD)
 
-.PHONY: Deploy
-Deploy:
-	@echo "install CEGUI"
-	@find $(CEGUI_DIR) -name "*.so" -exec cp -lf {} $(BUILD)/ \;
 
