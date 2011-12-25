531ff42c1ad97a5adb98d9310b51a3d0c2016466
Piotr Podusowski <podusowski@gmail.com>
2011-12-25 14:03:30 +0100

    refactor

--- Sources/Common/Game/Object/UnitTests/ShipTest.cpp
+++ Sources/Common/Game/Object/UnitTests/ShipTest.cpp
@@ -7,19 +7,34 @@
 
 using namespace testing;
 
-TEST(ShipTest, TestMoveByVector)
+class ShipTest : public Test
 {
-    boost::shared_ptr<Common::Game::IRustedTime> time(new RustedTimeStub);
-    Cake::DependencyInjection::forInterface<Common::Game::IRustedTime>().use(time);
+public:
+    void SetUp()
+    {
+         m_time.reset(new RustedTimeStub);
+         Cake::DependencyInjection::forInterface<Common::Game::IRustedTime>().use(m_time);
+    }
 
+    RustedTimeStub & getRustedTimeStub()
+    {
+        return dynamic_cast<RustedTimeStub &>(*m_time);
+    }
+
+private:
+    boost::shared_ptr<Common::Game::IRustedTime> m_time;
+};
+
+TEST_F(ShipTest, TestMoveByVector)
+{
     Common::Game::Object::Ship ship;
 
-    ON_CALL(dynamic_cast<RustedTimeStub&>(*time), getSeconds()).WillByDefault(Return(0));
+    ON_CALL(getRustedTimeStub(), getSeconds()).WillByDefault(Return(0));
 
     ship.setPosition(Common::Game::Position(0, 0, 0));
     ship.setCourse(Common::Game::Position(100, 0, 0));
 
-    EXPECT_CALL(dynamic_cast<RustedTimeStub&>(*time), getSeconds()).Times(1).WillOnce(Return(100));
+    EXPECT_CALL(getRustedTimeStub(), getSeconds()).Times(1).WillOnce(Return(100));
 
     ASSERT_EQ(Common::Game::Position(100, 0, 0), ship.getPosition());
 }
