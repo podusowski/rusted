e740292c797da19129e0b02059e819db2c5fee77
Piotr Podusowski <podusowski@gmail.com>
2011-12-25 10:39:06 +0100

    run_test.sh which is running SCT also

---
+++ run_tests.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ ! -d _build ]; then 
+    echo "no _build directory, project was not built or you are not in working directory"
+    exit 1
+fi
+
+function run_test()
+{
+    test_name=$1
+
+    stdout=`tempfile`
+    stderr=`tempfile`
+    ./$test_name > $stdout 2> $stderr
+    if [ "$?" = "0" ]; then
+        echo -n "pass "
+    else
+        echo -n "fail "
+        cat $stdout $stderr
+        result=1
+    fi
+    echo $test_name
+
+    rm $stdout $stderr
+}
+
+pushd _build > /dev/null
+
+result=0
+
+for i in *UT; do
+   if [ -f $i ]; then
+       run_test $i
+   fi
+done
+
+for i in *SCT; do
+   if [ -f $i ]; then
+       run_test $i
+   fi
+done
+
+popd > /dev/null
+
+exit $result
