39113a6b017d2bbeaa54593c01027512af952893
Piotr Podusowski <podusowski@gmail.com>
2012-02-12 21:38:54 +0100

    build: resources fix

--- Make/recipe.resources.mk
+++ Make/recipe.resources.mk
@@ -1,12 +1,9 @@
 define recipe_resources_TEMPLATE
 
-RESOURCES_DESTINATION:=$$(addprefix $$(BUILD)/,$(1))
-RESOURCES_SOURCE:=$$(addprefix $$(TARGET_BASE)/,$(1))
-
-$$(TARGET): $$(RESOURCES_DESTINATION)
-$$(RESOURCES_DESTINATION): $$(RESOURCES_SOURCE)
+$$(TARGET): $$(BUILD)/$(1)
+$$(BUILD)/$(1): $$(TARGET_BASE)/$(1)
 	@mkdir -p $$(BUILD)
 	@/bin/echo -e '$$(FONT_BOLD)cp$$(FONT_RESET) $(1)'
-	@cp -rt $$(BUILD)/ $$(RESOURCES_SOURCE)
+	@cp -rt $$(BUILD)/ $$(TARGET_BASE)/$(1)
 
 endef
