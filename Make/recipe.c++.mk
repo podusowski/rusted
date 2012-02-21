45a8d1d6eab802a3c9edabe65f9904256cdb6ff7
Piotr Podusowski <podusowski@gmail.com>
2012-02-21 14:33:21 +0100

    misc

--- Make/recipe.c++.mk
+++ Make/recipe.c++.mk
@@ -5,7 +5,7 @@ define recipe_c++_TEMPLATE
 
 $(1)/%.o: $(2)/%.cpp
 	@mkdir -p `dirname $$@`
-	@echo "$(FONT_BOLD)$(CC) $(FONT_RESET) $$(subst $(PWD)/,,$$<) "
+	@echo "$(FONT_BOLD)$(CC)$(FONT_RESET) $$(subst $(PWD)/,,$$<) "
 	@$$(CC) $$(CFLAGS) -c $$< -o $$@
 	@$$(CC) -MM $$(CFLAGS) $$< | sed 's!$$(notdir $$@):!$$@:!' > $$@.d
 
