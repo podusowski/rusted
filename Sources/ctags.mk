cc15a6d9959e66dabdc528dda0d0e335e1765831
Piotr Podusowski <podusowski@gmail.com>
2012-02-22 11:48:31 +0100

    misc

--- Sources/ctags.mk
+++ Sources/ctags.mk
@@ -3,4 +3,4 @@ TYPE:=custom
 
 ctags:
 	@/bin/echo -e '$(FONT_BOLD)ctags$(FONT_RESET)'
-	@ctags -R --c++-kinds=+p --fields=+miaS --extra=+q --exclude=".svn" -f tags . /usr/include/c++
+	@ctags -R --c++-kinds=+p --fields=+miaS --extra=+q --exclude=".svn" -f tags $(TARGET_BASE) /usr/include/c++
