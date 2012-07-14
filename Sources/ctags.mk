43d26e1869e088727b4435620be29cafed76eb58
Piotr Podusowski <wolverine@wolverine-K50IN.(none)>
2012-07-14 17:31:21 +0200

    ctags inside _build

--- Sources/ctags.mk
+++ Sources/ctags.mk
@@ -3,4 +3,4 @@ TYPE:=custom
 
 ctags:
 	@/bin/echo -e '$(FONT_BOLD)ctags$(FONT_RESET)'
-	@ctags -R --c++-kinds=+p --fields=+miaS --extra=+q --exclude=".svn" -f tags $(TARGET_BASE) /usr/include/c++ /usr/include/OGRE/
+	@ctags -R --c++-kinds=+p --fields=+miaS --extra=+q --exclude=".svn" -f tags $(TARGET_BASE) /usr/include/c++ /usr/include/OGRE/ $(BUILD)
