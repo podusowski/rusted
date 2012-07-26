4ec025f3eed75b6d7b554efaae45e19361d5b514
Piotr Podusowski <podusowski@gmail.com>
2012-07-26 18:19:10 +0200

    MyGUI compilation is unstable with -j :(

--- Sources/External/MyGUI/MyGUI.mk
+++ Sources/External/MyGUI/MyGUI.mk
@@ -14,4 +14,4 @@ $(BUILD)/MyGUI/lib/libMyGUIEngine.so:
 	cp $(TARGET_BASE)/$(PACKAGE_NAME) $(BUILD)/MyGUI
 	cd $(BUILD)/MyGUI && tar xf $(PACKAGE_NAME) && \
 		cmake . && \
-		make -j$(JOBS) MyGUIEngine MyGUI.OgrePlatform
+		make -j1 MyGUIEngine MyGUI.OgrePlatform
