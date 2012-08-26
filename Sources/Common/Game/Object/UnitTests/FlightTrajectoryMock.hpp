8c55151b098f0383384e45853a94236b719e9669
Piotr Podusowski <podusowski@gmail.com>
2012-08-26 22:41:39 +0200

    FlightTrajectoryMock added

---
+++ Sources/Common/Game/Object/UnitTests/FlightTrajectoryMock.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "Common/Game/Object/IFlightTrajectory.hpp"
+
+namespace Common
+{
+namespace Game
+{
+namespace Object
+{
+
+class FlightTrajectoryMock : public IFlightTrajectory
+{
+public:
+    MOCK_METHOD1(fly, void(Position));
+    MOCK_METHOD0(stop, void());
+    MOCK_METHOD1(setPosition, void(Position));
+    MOCK_METHOD0(getPosition, Position());
+    MOCK_METHOD1(setSpeed, void(unsigned));
+    MOCK_METHOD0(getDescription, Description());
+    MOCK_METHOD1(applyDescription, void(Description));
+};
+
+}
+}
+}
+
