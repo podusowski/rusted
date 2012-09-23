bcc0673e60a961d5c5f42cec2f58fedae9154a3b
Piotr Podusowski <podusowski@gmail.com>
2012-09-23 23:21:54 +0200

    run_sct script

---
+++ run_sct.sh
@@ -0,0 +1,24 @@
+tool=""
+while getopts 't:' opt; do
+    case $opt in
+        t)
+            tool=$OPTARG
+            ;;
+    esac
+done
+
+echo tool: $tool
+
+root=`pwd`
+pushd _build
+
+if [ "$tool" = "helgrind" ]; then
+    wrapper="valgrind --tool=helgrind --log-file=$root/helgrind-%p.xml --trace-children=yes --child-silent-after-fork=yes"
+    export SERVER_SCT_WAIT_FOR_APP_TIME=10
+    rm -f $root/helgrind*
+fi
+
+$wrapper ./ServerSCT $@
+
+popd
+
