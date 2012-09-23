03a482565e89ec8249c7d493c4a1ca74af8fde70
Piotr Podusowski <podusowski@gmail.com>
2012-09-23 21:32:32 +0200

    multithreaded smoke test

--- Sources/Server/SCT/SmokeSct.cpp
+++ Sources/Server/SCT/SmokeSct.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "Cake/Threading/Thread.hpp"
+
 #include "Core/Component.hpp"
 #include "Core/Connection.hpp"
 #include "Preconditions.hpp"
@@ -7,27 +9,58 @@
 
 using namespace Common::Messages;
 
+class SmokeSctRunnable : public Cake::Threading::IRunnable
+{
+public:
+    SmokeSctRunnable(SCT::Component & component, std::string user, std::string password): 
+        m_component(component),
+        m_user(user),
+        m_password(password)
+    {
+    }
+
+    void run()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            boost::shared_ptr<SCT::Connection> connection1 = authorizeUser(m_component, m_user, m_password); 
+
+            Common::Messages::FetchAvailableActions fetchAvailableActions;
+            fetchAvailableActions.shipId = 1;
+            connection1->send(fetchAvailableActions);
+            connection1->receive<Common::Messages::AvailableActions>();
+
+            Common::Messages::FocusObject focusObject;
+            focusObject.id = 1;
+            connection1->send(focusObject);
+
+            Common::Messages::SelectObject selectObject;
+            selectObject.id = 2;
+            connection1->send(selectObject);
+        }
+    }
+
+private:
+    SCT::Component & m_component; 
+    std::string m_user;
+    std::string m_password;
+};
+
 TEST(SmokeSct, Smoke)
 {
 	SCT::Component component("SampleDataBase.xml");
     component.start();
 
-    for (int i = 0; i < 100; i++)
-    {
-        boost::shared_ptr<SCT::Connection> connection1 = authorizeUser(component, "user1", "password"); 
+    SmokeSctRunnable runnable1(component, "user1", "password");
+    Cake::Threading::Thread thread1(runnable1);
 
-        Common::Messages::FetchAvailableActions fetchAvailableActions;
-        fetchAvailableActions.shipId = 1;
-        connection1->send(fetchAvailableActions);
-        auto availableActions = connection1->receive<Common::Messages::AvailableActions>();
+    SmokeSctRunnable runnable2(component, "user2", "password");
+    Cake::Threading::Thread thread2(runnable2);
 
-        Common::Messages::FocusObject focusObject;
-        focusObject.id = 1;
-        connection1->send(focusObject);
+    thread1.start();
+    thread2.start();
 
-        Common::Messages::SelectObject selectObject;
-        selectObject.id = 2;
-        connection1->send(selectObject);
-    }
+    thread1.join();
+    thread2.join();
 }
 
