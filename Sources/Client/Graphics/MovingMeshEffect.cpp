c76bea392d6465b1a7a625d520b0eb79cabdd9e0
Piotr Podusowski <podusowski@gmail.com>
2012-10-08 22:06:38 +0200

    work on moving mesh effect is moving on

--- Sources/Client/Graphics/MovingMeshEffect.cpp
+++ Sources/Client/Graphics/MovingMeshEffect.cpp
@@ -1,6 +1,28 @@
+#include <OgreSceneNode.h>
+#include <OgreEntity.h>
+
+#include "Cake/Diagnostics/Logger.hpp"
+
 #include "MovingMeshEffect.hpp"
 
 using namespace Client::Graphics;
+
+MovingMeshEffect::MovingMeshEffect(IGraphics & graphics,
+    Common::Game::Position start,
+    Common::Game::Position end) : 
+        m_graphics(graphics),
+        m_start(start),
+        m_end(end)
+{
+    LOG_DEBUG << "Creating MovingMeshEffect";
+
+    Ogre::SceneManager & scene = m_graphics.getSceneManager();
+    std::string mesh = "Cube.mesh";
+
+    m_entity = scene.createEntity(mesh);
+    m_node = scene.getRootSceneNode()->createChildSceneNode();
+    m_node->attachObject(m_entity);
+}
 
 void MovingMeshEffect::frameStarted()
 {
--- Sources/Client/Graphics/MovingMeshEffect.hpp
+++ Sources/Client/Graphics/MovingMeshEffect.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Common/Game/Position.hpp"
+#include "Common/Game/IRustedTime.hpp"
+#include "Common/Game/TimeValue.hpp"
+
+#include "IGraphics.hpp"
 #include "IEffect.hpp"
 
 namespace Client
@@ -10,8 +15,22 @@ namespace Graphics
 class MovingMeshEffect : public IEffect
 {
 public:
+    MovingMeshEffect(
+        IGraphics &, 
+        Common::Game::Position start,
+        Common::Game::Position end);
+
     void frameStarted(); 
     bool isAlive();
+
+private:
+    IGraphics & m_graphics;
+    Ogre::Entity * m_entity;
+    Ogre::SceneNode * m_node;
+    Common::Game::Position m_start;
+    Common::Game::Position m_end;
+    Common::Game::TimeValue m_startTime;
+    Cake::DependencyInjection::Inject<Common::Game::IRustedTime> m_time;
 };
 
 }
