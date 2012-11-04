fb7b6d10474bbb869feffc8ee16ffed6d16933d9
Piotr Podusowski <podusowski@gmail.com>
2012-11-04 23:09:30 +0100

    dead effects collector

--- Sources/Client/Graphics/Effects.cpp
+++ Sources/Client/Graphics/Effects.cpp
@@ -15,6 +15,8 @@ void Effects::frameStarted()
     {
         effect->frameStarted();
     }
+
+    gc();
 }
 
 void Effects::emitMovingMeshEffect(Common::Game::Position start, Common::Game::Position end, unsigned speed)
@@ -29,3 +31,21 @@ void Effects::addEffect(boost::shared_ptr<IEffect> effect)
 
     m_effects.push_back(effect);
 }
+
+void Effects::gc()
+{
+    auto it = m_effects.begin();
+    while (it != m_effects.end())
+    {
+        if ((*it)->isAlive())
+        {
+            it++;
+        }
+        else
+        {
+            LOG_DEBUG << "Collecting dead effect: " << TYPENAME(**it);
+            it = m_effects.erase(it);
+        }
+    }
+}
+
