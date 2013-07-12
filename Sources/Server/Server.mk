1d0c8548ac88e2871ba3fa900edeb4f3fd7d9346
Piotr Podusowski <podusowski@gmail.com>
2013-07-12 18:16:15 +0200

    unnesesary libs removed

--- Sources/Server/Server.mk
+++ Sources/Server/Server.mk
@@ -1,9 +1,9 @@
 TARGET:=Server
 TYPE:=application
 
-LDFLAGS:=-L$(BUILD) -lServer -lCommon -lCake -lpthread -lboost_system -lxml2 -lsoci_core
+LDFLAGS:=-L$(BUILD) -lServer -lCommon -lCake -lpthread -lsoci_core
 
-RESOURCES:=Server.cfg TestDataBase.xml ShipClasses.xml runTestServer TestDataBase.sqlite3
+RESOURCES:=Server.cfg runTestServer TestDataBase.sqlite3
 DEPENDENCIES+=libServer.a libCommon.a libCake.a
 
 $(TARGET_BASE)/TestDataBase.sqlite3: $(TARGET_BASE)/DataBaseSchema.sql $(TARGET_BASE)/TestDataBase.sql
