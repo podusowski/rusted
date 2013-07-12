83571ee31bb38648f9cce0fb78ddba5b6857c06e
Piotr Podusowski <podusowski@gmail.com>
2013-07-12 17:15:47 +0200

    soci mk fix

--- Sources/External/SOCI/SOCI.mk
+++ Sources/External/SOCI/SOCI.mk
@@ -7,6 +7,7 @@ SOCI_PACKAGE_NAME:=soci-3.2.1.tar.gz
 SOCI: $(BUILD)/libsoci_core.so.3.2.1
 
 $(BUILD)/libsoci_core.so.3.2.1: $(TARGET_BASE)/$(SOCI_PACKAGE_NAME)
+	rm -rf $(BUILD)/SOCI
 	mkdir -p $(BUILD)/SOCI
 	cp $(TARGET_BASE)/$(SOCI_PACKAGE_NAME) $(BUILD)/SOCI
 	cd $(BUILD)/SOCI && tar xf $(SOCI_PACKAGE_NAME) && \
