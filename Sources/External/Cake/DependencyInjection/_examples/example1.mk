b5459ad98d5dd2fd896ae0c8e2869de89a8211cc
Piotr Podusowski <podusowski@gmail.com>
2013-07-14 22:26:55 +0200

    non rusted crucial stuff removed

--- Sources/External/Cake/DependencyInjection/_examples/example1.mk
+++
@@ -1,4 +0,0 @@
-TARGET:=example1
-TYPE:=application
-CFLAGS+=-I$(TARGET_BASE)/../..
-SOURCES+=example1.cpp
--- Sources/External/Cake/DependencyInjection/_examples/example2.mk
+++
@@ -1,4 +0,0 @@
-TARGET:=example2
-TYPE:=application
-CFLAGS+=-I$(TARGET_BASE)/../..
-SOURCES+=example2.cpp
