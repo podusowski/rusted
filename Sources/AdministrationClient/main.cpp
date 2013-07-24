76b952a992baaa48ed7f6c2583dc93e4f9efb9c9
Piotr Podusowski <podusowski@gmail.com>
2013-07-24 23:46:21 +0200

    admin client misc

--- Sources/AdministrationClient/main.cpp
+++ Sources/AdministrationClient/main.cpp
@@ -1,3 +1,27 @@
-int main()
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "Common/Messages/Messages.hpp"
+#include "Cake/Networking/Socket.hpp"
+
+int main(int argc, char * argv[])
 {
+    std::vector<std::string> args(argv, argv + argc);
+
+    if (args.size() < 2)
+    {
+        std::cerr << "no administration socket path specified";
+        return 1;
+    }
+
+    auto socket = Cake::Networking::Socket::connectToUnixSocket(args[1]);
+
+    while (true)
+    {
+        std::string command;
+        std::cout << "rusted> ";
+        std::getline(std::cin, command);
+    }
 }
+
