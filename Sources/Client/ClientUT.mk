a6a52933a4517d9ddfcbdb961096d61af029f7ca
Piotr Podusowski <podusowski@gmail.com>
2013-08-07 18:34:51 +0200

    libboost_system dropped from client

--- Sources/Client/ClientUT.mk
+++ Sources/Client/ClientUT.mk
@@ -11,14 +11,13 @@ CFLAGS+=-I$(TARGET_BASE)/../External
 CFLAGS+=-I$(TARGET_BASE)/../External/GoogleTest
 CFLAGS+=-I/usr/include/libxml2
 
-LDFLAGS+=-L. -L$(BUILD) -lpthread -lboost_system
+LDFLAGS+=-L. -L$(BUILD) -lpthread
 LDFLAGS+=-lClient
 LDFLAGS+=-lCommon
 LDFLAGS+=-lCake
 LDFLAGS+=-lgtest-static
 LDFLAGS+=-lOgreMain
 LDFLAGS+=-lOIS
-LDFLAGS+=-lboost_system
 
 DEPENDENCIES+=libClient.a
 DEPENDENCIES+=libCommon.a
