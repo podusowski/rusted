2f0ffd74db080b16998f29dff59a0ebf2c5f9bd4
Piotr Podusowski <podusowski@gmail.com>
2013-09-29 13:01:33 +0200

    misc

--- Sources/Server/Game/UnitTests/UniverseDataBaseFacadeMock.hpp
+++ Sources/Server/Game/UnitTests/UniverseDataBaseFacadeMock.hpp
@@ -12,7 +12,6 @@ namespace Game
 class UniverseDataBaseFacadeMock : public IUniverseDataBaseFacade
 {
 public:
-    MOCK_METHOD1(create, boost::shared_ptr<Common::Game::Object::ObjectBase>(const soci::row &));
     MOCK_METHOD2(createShip, boost::shared_ptr<Common::Game::Object::ObjectBase>(unsigned, unsigned));
 };
 
