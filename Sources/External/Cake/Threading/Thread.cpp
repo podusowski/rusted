510cd2657b6a012a84490c502e611712df17f530
Piotr Podusowski <podusowski@gmail.com>
2013-09-30 21:15:55 +0200

    cleanup

--- Sources/External/Cake/Threading/Thread.cpp
+++ Sources/External/Cake/Threading/Thread.cpp
@@ -9,8 +9,8 @@
 using namespace Cake::Threading;
 
 Thread::Thread(IRunnable & p_runnable) :
-	m_isRunning(false),
-	m_runnable(p_runnable)
+    m_isRunning(false),
+    m_runnable(p_runnable)
 {
 }
 
@@ -20,8 +20,8 @@ Thread::~Thread()
 
 void Thread::start()
 {
-	m_isRunning = true;
-	int ret = pthread_create(&m_thread, NULL, Thread::m_run, (void *)this);
+    m_isRunning = true;
+    int ret = pthread_create(&m_thread, NULL, Thread::s_run, (void *)this);
 
     if (ret == EAGAIN)
     {
@@ -36,11 +36,11 @@ void Thread::start()
         throw std::runtime_error("no permission to create a thread");
     }
     else if (ret != 0)
-	{
-	    std::stringstream ss;
-	    ss << "error during pthread_create: " << ret;
+    {
+        std::stringstream ss;
+        ss << "error during pthread_create: " << ret;
         throw std::runtime_error(ss.str());
-	}
+    }
 }
 
 void Thread::join()
@@ -50,10 +50,10 @@ void Thread::join()
 
 void Thread::wait(int secs, int mili)
 {
-	timespec l_ts;
-	l_ts.tv_sec = secs;
-	l_ts.tv_nsec = mili * 1000 * 1000;
-	nanosleep(&l_ts, 0);
+    timespec l_ts;
+    l_ts.tv_sec = secs;
+    l_ts.tv_nsec = mili * 1000 * 1000;
+    nanosleep(&l_ts, 0);
 }
 
 unsigned Thread::self()
@@ -61,26 +61,26 @@ unsigned Thread::self()
     return pthread_self();
 }
 
-void * Thread::m_run(void * threadCtx)
+void * Thread::s_run(void * threadCtx)
 {
     Thread & thread = *static_cast<Thread *>(threadCtx);
 
-	try
-	{
-	    thread.m_runnable.run();
-	}
-	catch (...)
-	{
-	}
+    try
+    {
+        thread.m_runnable.run();
+    }
+    catch (...)
+    {
+    }
 
     // basically bool should be atomic but we'll be valgrind clean
-	thread.m_isRunningLock.aquire();
-	thread.m_isRunning = false;
-	thread.m_isRunningLock.release();
+    thread.m_isRunningLock.aquire();
+    thread.m_isRunning = false;
+    thread.m_isRunningLock.release();
 
-	pthread_detach(pthread_self());
+    pthread_detach(pthread_self());
 
-	return 0;
+    return 0;
 }
 
 bool Thread::isRunning()
--- Sources/External/Cake/Threading/Thread.hpp
+++ Sources/External/Cake/Threading/Thread.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
-#include <pthread.h>
+#ifdef _WIN32
+    #include <windows.h>
+    #error sorry, WIN32 is not supported
+#else
+    #include <pthread.h>
+#endif
 
 #include "IRunnable.hpp"
 #include "Mutex.hpp"
 
-namespace Cake 
+namespace Cake
 {
 namespace Threading
 {
@@ -19,23 +24,25 @@ public:
         IRunnable * runnable;
     };
 
-	Thread(IRunnable &);
-	~Thread();
+    Thread(IRunnable &);
+    ~Thread();
 
-	void start();
-	void join();
-	bool isRunning();
+    void start();
+    void join();
+    bool isRunning();
 
-	static void wait(int secs, int mili = 0);
-	static unsigned self();
+    static void wait(int secs, int mili = 0);
+    static unsigned self();
+
 private:
-	static void * m_run(void *);
+    static void * s_run(void *);
 
     bool m_isRunning;
-	IRunnable & m_runnable;
-	pthread_t m_thread;
-	Mutex m_isRunningLock;
+    IRunnable & m_runnable;
+    pthread_t m_thread;
+    Mutex m_isRunningLock;
 };
 
 }
 }
+
