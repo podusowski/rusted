29e1c00d14582e192e483efb87e37cad639a536c
Piotr Podusowski <podusowski@gmail.com>
2013-10-06 20:03:15 +0200

    patched MyGUI's cmake scripts to properly detect ogre

--- Sources/External/MyGUI/MyGUI.mk
+++ Sources/External/MyGUI/MyGUI.mk
@@ -1,7 +1,7 @@
 TARGET:=MyGUI
 TYPE:=custom
 
-PACKAGE_NAME=$(TARGET_BASE)/MyGUI_3.2.0.tar.gz
+PACKAGE_NAME=$(TARGET_BASE)/MyGUI_3.2.0_patched.tar.gz
 MYGUI_DIR=$(BUILD)/MyGUI
 
 .PHONY: MyGUI
@@ -10,7 +10,7 @@ MyGUI: $(BUILD)/libMyGUIEngine.so
 $(BUILD)/libMyGUIEngine.so: $(BUILD)/MyGUI/lib/libMyGUIEngine.so
 	cp -v $(MYGUI_DIR)/_build/lib/* $(BUILD)
 
-$(MYGUI_DIR)/lib/libMyGUIEngine.so:
+$(MYGUI_DIR)/lib/libMyGUIEngine.so: OGRE
 	mkdir -p $(MYGUI_DIR)
 	cd $(MYGUI_DIR) && tar xf $(PACKAGE_NAME)
 	mkdir -p $(MYGUI_DIR)/_build
