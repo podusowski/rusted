9ea09507e75af0f94e07477cdf7acb841f6b0532
Piotr Podusowski <podusowski@gmail.com>
2013-10-06 19:31:25 +0200

    mygui building with shipped ogre

--- Sources/External/MyGUI/MyGUI.mk
+++ Sources/External/MyGUI/MyGUI.mk
@@ -1,19 +1,19 @@
 TARGET:=MyGUI
 TYPE:=custom
 
-PACKAGE_NAME=$(TARGET_BASE)/MyGUI.tar.gz
+PACKAGE_NAME=$(TARGET_BASE)/MyGUI_3.2.0.tar.gz
 MYGUI_DIR=$(BUILD)/MyGUI
 
 .PHONY: MyGUI
 MyGUI: $(BUILD)/libMyGUIEngine.so
 
 $(BUILD)/libMyGUIEngine.so: $(BUILD)/MyGUI/lib/libMyGUIEngine.so
-	cp -v $(BUILD)/MyGUI/lib/* $(BUILD)
+	cp -v $(MYGUI_DIR)/_build/lib/* $(BUILD)
 
 $(MYGUI_DIR)/lib/libMyGUIEngine.so:
 	mkdir -p $(MYGUI_DIR)
-	cd $(MYGUI_DIR) && \
-		tar xf $(PACKAGE_NAME) && \
-		cmake . && \
+	mkdir -p $(MYGUI_DIR)/_build
+	cd $(MYGUI_DIR)/_build && \
+		cmake .. -DMYGUI_RENDERSYSTEM=2 -DOGRE_SOURCE=$(OGRE_DIR) -DOGRE_BUILD=$(OGRE_DIR)/_build && \
 		make -j1 MyGUIEngine MyGUI.OgrePlatform
 
--- Sources/External/OGRE/OGRE.mk
+++ Sources/External/OGRE/OGRE.mk
@@ -4,3 +4,25 @@ TYPE:=custom
 OGRE_PACKAGE_NAME:=$(TARGET_BASE)/ogre_src_v1-8-1.tar.bz2
 OGREDEPS_PACKAGE_NAME:=$(TARGET_BASE)/ogredeps.tar.gz
 
+OGREDEPS_DIR:=$(BUILD)/OGREDEPS
+OGRE_DIR:=$(BUILD)/OGRE/ogre_src_v1-8-1/
+
+.PHONY: OGRE OGREDEPS
+
+OGRE: OGREDEPS
+
+OGREDEPS:
+	mkdir -p $(OGREDEPS_DIR)
+	cd $(OGREDEPS_DIR) && \
+		tar xf $(OGREDEPS_PACKAGE_NAME) && \
+		cmake . && \
+		make
+
+OGRE:
+	mkdir -p $(OGRE_DIR)
+	cd $(OGRE_DIR)/.. && tar xf $(OGRE_PACKAGE_NAME)
+	mkdir -p $(OGRE_DIR)/_build
+	cd $(OGRE_DIR)/_build && \
+		cmake .. && \
+		make OgreMain RenderSystem_GL Plugin_OctreeSceneManager Plugin_ParticleFX
+
