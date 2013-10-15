6eb8ade696a26cecea14095570cad0608c373545
Piotr Podusowski <podusowski@gmail.com>
2013-10-15 21:00:54 +0200

    all is building now

--- Make/recipe.resources.mk
+++ Make/recipe.resources.mk
@@ -4,6 +4,6 @@ $$(TARGET): $$(BUILD)/$(1)
 $$(BUILD)/$(1): $$(TARGET_BASE)/$(1)
 	@mkdir -p $$(BUILD)
 	@/bin/echo -e '$$(FONT_BOLD)cp$$(FONT_RESET) $(1)'
-	@cp -rt $$(BUILD)/ $$(TARGET_BASE)/$(1)
+	@cp -rt $$(BUILD)/ $(TARGET_BASE)/$(1)
 
 endef
