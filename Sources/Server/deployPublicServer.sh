3d48b352136994d96644bc41719c02958d82aba6
Piotr Podusowski <podusowski@gmail.com>
2014-01-05 00:56:19 +0100

    server deploy script

--- Sources/Server/deployPublicServer.sh
+++ Sources/Server/deployPublicServer.sh
@@ -1,8 +1,74 @@
-function deploy()
+#!/bin/bash
+
+root_directory=`dirname $0`
+rusted_directory=/var/rusted/
+pid_file=$rusted_directory/Server.pid
+database_file=$rusted_directory/database.sqlite3
+
+function stop()
 {
-    local rusted_directory=/var/rusted/
-    cp -v Server $rusted_directory
+    if [ -f $pid_file ]; then
+        echo "stopping server"
+        kill `cat $pid_file`
+        rm $pid_file
+    else
+        echo "server is not running"
+    fi
 }
 
-deploy
+function deploy()
+{
+    if [ ! -e $rusted_directory ]; then
+        echo "$rusted_directory doesn't exists and I cannot create it for you :("
+        exit 1
+    fi
+
+    if [ -f $pid_file ]; then
+        echo "pid file exists, cannot start the server"
+        exit 1
+    fi
+
+    cp -v $root_directory/Server $rusted_directory
+    cp -v $root_directory/libsoci* $rusted_directory
+
+    echo "generating database"
+    if [ -f $database_file ]; then
+        echo "database already exists"
+    else
+        database_schema=$root_directory/DataBaseSchema.sql
+        cat $database_schema | sqlite3 $database_file
+    fi
+}
+
+function start()
+{
+    local server_arguemnts+="--network.port 1987 "
+    local server_arguemnts+="--network.administration_socket_path $rusted_directory/administrator.socket "
+    local server_arguemnts+="--database.url sqlite3://$database_file "
+
+    echo "starting Server with arguments: $server_arguemnts"
+    ( LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$rusted_directory nohup $rusted_directory/Server $server_arguemnts > /var/rusted/Server.log & echo "$!" > $pid_file )
+
+    echo "log available in $rusted_directory/Server.log"
+}
+
+function status()
+{
+    echo "showing status, to do some other thing use: deploy, start or stop"
+
+    if [ -e $pid_file ]; then
+        ps `cat $pid_file`
+    else
+        echo server is not running
+    fi
+}
+
+action=$1
+
+case "$action" in
+    start) start ;;
+    stop) stop ;;
+    deploy) deploy ;;
+    *) status ;;
+esac
 
