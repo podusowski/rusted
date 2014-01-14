6bc616385734522e749dc3ea7a0485a1bdaebac1
Piotr Podusowski <podusowski@gmail.com>
2014-01-14 21:52:46 +0100

    objects removed from default database

--- Sources/Server/DataBaseShips.sql
+++ Sources/Server/DataBaseShips.sql
@@ -1,19 +1,3 @@
-/*                         id   type        class  model             x     y     z      owner integrity carbon helium */
-INSERT OR REPLACE INTO objects VALUES(1,   "Ship",     1,     NULL,             1,    2000, 1,     1,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(2,   "Ship",     2,     NULL,             1,    4000, 1,     1,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(3,   "Ship",     3,     NULL,             1,    5000, 1,     1,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(4,   "Ship",     4,     NULL,             1,    6000, 1,     1,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(5,   "Ship",     5,     NULL,             1,    7000, 1,     1,    100,      0,     0);
-
-INSERT OR REPLACE INTO objects VALUES(11,  "Ship",     1,     NULL,             1,    2000, 10000, 2,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(12,  "Ship",     2,     NULL,             1,    4000, 10000, 2,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(13,  "Ship",     3,     NULL,             1,    5000, 10000, 2,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(14,  "Ship",     4,     NULL,             1,    6000, 10000, 2,    100,      0,     0);
-INSERT OR REPLACE INTO objects VALUES(15,  "Ship",     5,     NULL,             1,    7000, 10000, 2,    100,      0,     0);
-
-INSERT OR REPLACE INTO objects VALUES(100, "Asteroid", NULL,  "Asteroid.model", 2000, 1,    1,     NULL, 100,      1000,  1000);
-INSERT OR REPLACE INTO objects VALUES(101, "Asteroid", NULL,  "Asteroid.model", 2000, 1000, 1,     NULL, 100,      1000,  1000);
-
 /*     OR REPLACE                          id   speed   max_integrity  name             model                 max_capacity  required_carbon  required_helium */
 INSERT OR REPLACE INTO ship_classes VALUES(1,   500,    1000,          "Figter",        "Fighter.model",      100,          10,              5);
 INSERT OR REPLACE INTO ship_classes VALUES(5,   5000,   100,           "Carnwennan",    "Carnwennan.model",   100,          10,              5);
