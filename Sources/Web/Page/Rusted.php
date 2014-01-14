37c25823f8a59b7f461cc78aafa0cc098ada04b6
Piotr Podusowski <podusowski@gmail.com>
2014-01-14 21:35:16 +0100

    add ship when registering the player

--- Sources/Web/Page/Rusted.php
+++ Sources/Web/Page/Rusted.php
@@ -7,7 +7,7 @@ class Rusted
         $this->rusted_directory = "/var/rusted";
         $this->pid_file = "/var/rusted/Server.pid";
         $this->db = new PDO("sqlite:" . $this->rusted_directory . "/database.sqlite3");
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     function online()
@@ -28,8 +28,31 @@ class Rusted
     function register($login, $password)
     {
         $passwordHash = sha1($password);
+
+        $this->db->beginTransaction();
+
         $insert = $this->db->prepare("INSERT INTO users (login, password) VALUES(:login, :password)");
-        return $insert->execute(array(':login' => $login, ':password' => $passwordHash));
+        $insert->execute(array(':login' => $login, ':password' => $passwordHash));
+
+        $insert = $this->db->prepare("
+            INSERT INTO objects (type, class, x, y, z, owner, integrity, carbon, helium)
+            VALUES(:type, :class, :x, :y, :z, :owner, :integrity, :carbon, :helium)");
+
+        $playerId = $this->db->lastInsertId();
+
+        $insert->execute(array(
+            ':type' => "Ship",
+            ':class' => 1,
+            ":x" => 100,
+            ":y" => 100,
+            ":z" => 100,
+            ":owner" => $playerId,
+            ":integrity" => 100,
+            ":carbon" => 0,
+            ":helium" => 0
+        ));
+
+        $this->db->commit();
     }
 }
 
