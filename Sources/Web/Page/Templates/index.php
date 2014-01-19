8ec1cd68416433a805448c90fa7d641f1f40d1b9
Piotr Podusowski <podusowski@gmail.com>
2014-01-19 15:15:39 +0100

    some web stuff

--- Sources/Web/Page/Templates/index.php
+++ Sources/Web/Page/Templates/index.php
@@ -4,9 +4,21 @@
 
 <h2>Welcome</h2>
 <p>
-Welcome in Rusted alpha. Rusted is multiplayer online game taking action in outer space.
+Welcome in Rusted alpha. Rusted is a multiplayer online game taking action in outer space.
 </p>
 
+<h2>FAQ</h2>
+<h3>When it's going to be ready?</h3>
+<p>
+Not sure, Rusted is developed by a team which consists of... me :) Also, it's not a part of any company so I'm working on the game only on my free time.
+</p>
+
+<h3>Will it be free?</h3>
+<p>
+This isn't decided yet, for sure, I'll have to pay at least for a server so it'll have to earn money in one way or another, maybe some ads the web site will be enough, will see.
+</p>
+
+<h2>Beta server status</h2>
 <p>server status: <?= $this->rusted->online() ? "online" : "offline" ?></p>
 <p>users registered: <?= $this->rusted->users_registered(); ?></p>
 
