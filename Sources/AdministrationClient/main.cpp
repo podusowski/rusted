09a295445c37329eb0969e2ee2f2ac76125977fc
Piotr Podusowski <podusowski@gmail.com>
2014-01-20 21:14:25 +0100

    administration client is pretty much ready, no reading though

--- Sources/AdministrationClient/main.cpp
+++ Sources/AdministrationClient/main.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <vector>
 
-#include "Common/Messages/Messages.hpp"
+#include "Cake/Networking/Protocol/CakeWriteBuffer.hpp"
 #include "Cake/Networking/Socket.hpp"
+#include "Common/Messages/Messages.hpp"
 
 int main(int argc, char * argv[])
 {
@@ -17,11 +18,24 @@ int main(int argc, char * argv[])
 
     auto socket = Cake::Networking::Socket::connectToUnixSocket(args[1]);
 
-    while (true)
+    while (std::cin.good())
     {
         std::string command;
         std::cout << "rusted> ";
         std::getline(std::cin, command);
+
+        try
+        {
+            auto message = Common::Messages::MessageFactory::create(command);
+            std::cout << "sending " << *message << "\n";
+
+            Cake::Networking::Protocol::CakeWriteBuffer buffer(*socket);
+            message->serialize(buffer);
+        }
+        catch (const std::exception & ex)
+        {
+            std::cout << ex.what() << "\n";
+        }
     }
 }
 
